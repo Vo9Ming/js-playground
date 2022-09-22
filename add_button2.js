a38b16301b285454dde46f4a73ba668d9edfc895
Vo9Ming <kimminj2017@gmail.com>
2022-09-22 13:55:45 +0900

    DOM으로 버튼 추가해보기 (js파일)
    DOM을 이용해서 JS로 버튼 추가해보기

--- add_button2.js
+++ add_button2.js
@@ -11,6 +11,6 @@ btn2.onclick = readInput;
 // 함수 호출인 readInput() 이 아닌 함수 연결로 해주어야 한다.
 
 var test = document.getElementById("test");
-test.appendChild(document.createElement("br"));
+//test.appendChild(document.createElement("br"));
 // br 추가
 test.appendChild(btn2);
